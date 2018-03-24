18f5e56740f9f8b3e084b189309e721b44935c2f
Guillermo Soler Poquet <solerguillermo@hotmail.com>
2018-03-25 00:43:21 +0100

    Created function to load scripts and registered admin styles

--- wp-content/themes/sunset theme/inc/enqueue.php
+++ wp-content/themes/sunset theme/inc/enqueue.php
@@ -2,10 +2,14 @@
 
   /*
   @package Sunset Theme
-    ====================
-      ADMIN PAGE
-    =====================
+    ==============================
+      ADMIN ENQUEUE FUNCTIONS
+    ===============================
   */
-  
+
+  function sunset_load_admin_scripts() {
+    wp_register_style( 'sunset_admin', get_template_directory_uri() . '/css/sunset.admin.css', array(), '1.0.0', 'all' );
+  }
+
 
 ?>
