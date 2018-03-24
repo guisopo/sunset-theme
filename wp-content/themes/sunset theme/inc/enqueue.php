884a9b98ec1aadfc367b98b0fbcab57237a8cd23
Guillermo Soler Poquet <solerguillermo@hotmail.com>
2018-03-25 00:56:00 +0100

    Enqueued admin styles

--- wp-content/themes/sunset theme/inc/enqueue.php
+++ wp-content/themes/sunset theme/inc/enqueue.php
@@ -9,6 +9,7 @@
 
   function sunset_load_admin_scripts() {
     wp_register_style( 'sunset_admin', get_template_directory_uri() . '/css/sunset.admin.css', array(), '1.0.0', 'all' );
+    wp_enqueue_style( 'sunset_admin');
   }
 
 
