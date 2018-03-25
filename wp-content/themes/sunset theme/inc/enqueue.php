a0d9c665531188b129f235d1a567c63ba96276ac
Guillermo Soler Poquet <solerguillermo@hotmail.com>
2018-03-25 12:31:59 +0200

    Registerd and enqueued sunset.admin.js file

--- wp-content/themes/sunset theme/inc/enqueue.php
+++ wp-content/themes/sunset theme/inc/enqueue.php
@@ -14,6 +14,9 @@
     }
     wp_register_style( 'sunset_admin', get_template_directory_uri() . '/css/sunset.admin.css', array(), '1.0.0', 'all' );
     wp_enqueue_style( 'sunset_admin');
+
+    wp_register_script( 'sunset-admin-script', get_template_directory_uri() . '/js/sunset.admin.js', array('jquery'), '1.0.0', true );
+    wp_enqueue_script( 'sunset-admin-script');
   }
 
   add_action( 'admin_enqueue_scripts', 'sunset_load_admin_scripts');
