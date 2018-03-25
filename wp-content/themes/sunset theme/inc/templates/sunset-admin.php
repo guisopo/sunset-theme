5a3f918e35e55d08129cb48ce5bd0c1a0858a500
Guillermo Soler Poquet <solerguillermo@hotmail.com>
2018-03-25 11:12:46 +0200

    Created the structure for the sidebar preview

--- wp-content/themes/sunset theme/inc/templates/sunset-admin.php
+++ wp-content/themes/sunset theme/inc/templates/sunset-admin.php
@@ -1,5 +1,22 @@
 <h1>Sunset Theme Options</h1>
 <?php settings_errors(); ?>
+
+<?php
+  $firstName = esc_attr( get_option( 'first_name') );
+  $lastName = esc_attr( get_option( 'last_name') );
+  $fullName = $firstName . ' ' . $lastName;
+  $description = esc_attr( get_option( 'user_description'));
+?>
+
+<div class="sunset-sidebar-preview">
+  <div class="sunset-sidebar">
+    <h1 class="sunset-username"><?php print $fullName ?></h1>
+    <h2 class="sunset-description"><?php print $description ?></h2>
+    <div class="icons-wrapper">
+    </div>
+  </div>
+</div>
+
 <!-- Method will always be post -->
 <form method="post" action="options.php">
   <?php settings_fields( 'sunset-settings-group' ); ?>
