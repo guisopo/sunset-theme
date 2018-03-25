7f31660fe621f731ed065eeb75100e3a3019105f
Guillermo Soler Poquet <solerguillermo@hotmail.com>
2018-03-25 01:31:04 +0100

    Added conditional to apply admin styles just to admin section

--- wp-content/themes/sunset theme/inc/enqueue.php
+++ wp-content/themes/sunset theme/inc/enqueue.php
@@ -7,7 +7,11 @@
     ===============================
   */
 
-  function sunset_load_admin_scripts() {
+  function sunset_load_admin_scripts( $hook ) {
+    if('toplevel_page_guisopo_sunset' != $hook) {
+      // If they are not equal block the execution of the script
+      return;
+    }
     wp_register_style( 'sunset_admin', get_template_directory_uri() . '/css/sunset.admin.css', array(), '1.0.0', 'all' );
     wp_enqueue_style( 'sunset_admin');
   }
