4206689421938a8fdb74d0a9e75be87b9ce3bf61
Guillermo Soler Poquet <solerguillermo@hotmail.com>
2018-03-25 12:39:05 +0200

    Added media uploader hook

--- wp-content/themes/sunset theme/inc/enqueue.php
+++ wp-content/themes/sunset theme/inc/enqueue.php
@@ -15,6 +15,8 @@
     wp_register_style( 'sunset_admin', get_template_directory_uri() . '/css/sunset.admin.css', array(), '1.0.0', 'all' );
     wp_enqueue_style( 'sunset_admin');
 
+    wp_enqueue_media();
+
     wp_register_script( 'sunset-admin-script', get_template_directory_uri() . '/js/sunset.admin.js', array('jquery'), '1.0.0', true );
     wp_enqueue_script( 'sunset-admin-script');
   }
