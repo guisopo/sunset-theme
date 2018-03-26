1361e7562f829d067fc4162d65690bab68d85244
Guillermo Soler Poquet <solerguillermo@hotmail.com>
2018-03-26 10:05:35 +0200

    Created page first structure

---
+++ wp-content/themes/sunset theme/inc/templates/sunset-theme-support.php
@@ -0,0 +1,14 @@
+<h1>Sunset Theme Support</h1>
+<?php settings_errors(); ?>
+
+<?php
+  // $profilePicture = esc_attr( get_option( 'profile_picture') );
+?>
+
+
+<!-- Method will always be post -->
+<form method="post" action="options.php">
+  <?php //settings_fields( 'sunset-settings-group' ); ?>
+  <?php //do_settings_sections( 'guisopo_sunset' ); ?>
+  <?php //submit_button(); ?>
+</form>
