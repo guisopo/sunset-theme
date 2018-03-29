e925c501189128d312d6f3f53d2878b7b49eba67
Guillermo Soler Poquet <solerguillermo@hotmail.com>
2018-03-29 18:39:00 +0200

    created contact post type

--- wp-content/themes/sunset theme/functions.php
+++ wp-content/themes/sunset theme/functions.php
@@ -3,5 +3,6 @@
   require get_template_directory() . '/inc/functions-admin.php';
   require get_template_directory() . '/inc/enqueue.php';
   require get_template_directory() . '/inc/theme-support.php';
+  require get_template_directory() . '/inc/custom-post-type.php';
 
 ?>
--- wp-content/themes/sunset theme/inc/theme-support.php
+++ wp-content/themes/sunset theme/inc/theme-support.php
@@ -2,9 +2,9 @@
 
   /*
   @package Sunset Theme
-    ====================
+    =======================
         THEME SUPPORT PAGE
-    =====================
+    ========================
   */
 
   $options = get_option('post_formats');
@@ -12,9 +12,7 @@
   $formats = array('aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat');
   $output = array();
   foreach ($formats as $format) {
-
     $output[] = (isset($options[$format]) && $options[$format] == 1 ? $format : '');
-
     if( !empty($options)) {
       add_theme_support( 'post-formats', $output);
     }
